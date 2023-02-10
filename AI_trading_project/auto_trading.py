81be2a609c7cf2394a5a99e2bef8c473e263be97
woobin <kwonwoobin@kakao.com>
2023-02-10 22:36:58 +0900

    modified codefile

--- AI_trading_project/auto_trading.py
+++ AI_trading_project/auto_trading.py
@@ -251,27 +251,6 @@ URL_BASE = _cfg['URL_BASE']
 
 
 
-
-#토큰 지정시 토큰 타입("Bearer") 지정 필요. 즉, 발급받은 접근토큰 앞에 앞에 "Bearer" 붙여서 호출
-def get_current_price(code):
-    """현재가 조회"""
-    # 현재 기업별 주식 체크
-    PATH = "uapi/domestic-stock/v1/quotations/inquire-price"
-    URL = f"{URL_BASE}/{PATH}"
-    headers = {"Content-Type":"application/json", 
-            "authorization": f"Bearer {ACCESS_TOKEN}",
-            "appKey":APP_KEY,
-            "appSecret":APP_SECRET,
-            "tr_id":"FHKST01010100"}
-    params = {
-    "fid_cond_mrkt_div_code":"J",
-    "fid_input_iscd":code,
-    }
-    res = requests.get(URL, headers=headers, params=params)
-    #데이터 확인해보기(res.json()) 형태 결과값 사용해보기
-    print(res.json()['output']['stck_prpr']) 
-    return int(res.json()['output']['stck_prpr'])
-
 def get_stock_balance():
     """주식 잔고조회(모델별 통합계좌 운용)"""
     PATH = "uapi/domestic-stock/v1/trading/inquire-balance"
